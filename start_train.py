5f069ad4ed00d0e08e9b558f2c24fc972d7580fc
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2024-11-27 17:11:23 +0200

    Updated gitignore and test_training_notebook

--- start_train.py
+++ start_train.py
@@ -7,14 +7,14 @@ def parse_args():
     parse.add_argument('--pretained', type=str, default=None, help='pretained model')
     parse.add_argument('--data_dir', type=str, default=None, help='the dir to data')
     parse.add_argument('--device', type=str, default="0", help='device on which training will be held')
-    parse.add_argument('--epochs', type=str, default="1", help='number of training epochs')
+    parse.add_argument('--epochs', type=int, default=1, help='number of training epochs')
     args = parse.parse_args()
     return args
 
 def main():
     args = parse_args()
     model = YOLO(args.model)#.load(args.pretained)
-    model.train(data=args.data_dir, device=args.device)
+    model.train(data=args.data_dir, device=args.device, epochs=args.epochs)
 
 if __name__ == '__main__':
     main()
